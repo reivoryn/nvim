bec48d2a8ea8a3c30b3156b517d9174842f83b0c
Muhammad Rifai <reicursion@proton.me>
2026-06-06 08:47:12 +0700

    fix: treesitter issue

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,7 @@ return {
   build = ":TSUpdate", -- Command to run after the plugin is installed to update Treesitter parsers
   commit = "42fc28ba918343ebfd5565147a42a26580579482", -- Latest releases had so many breaking changes
   event = { "BufReadPre", "BufNewFile" }, -- Load this plugin when opening a buffer
-
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  lazy = false,
 
   opts = {
     ensure_installed = {
